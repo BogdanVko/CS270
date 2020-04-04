4ca0934906aea1695622d5b6c877f8eda1f3aab1
Bogdan <beeer01net@gmail.com>
2020-04-04 12:58:43 -0600

    P7 setup

---
+++ P7/printnum.asm
@@ -0,0 +1,104 @@
+; Author: Fritz Sieker
+;
+; ------------------------------------------------------------------------
+; Begin reserved section - do not change ANYTHING is this section
+; except the .FILL values of option, data1, data2
+
+               .ORIG x3000
+               BR Main
+
+option         .FILL 0          ; select routine to test
+data1          .FILL 0          ; use ONLY for testing
+data2          .FILL 0          ; use ONLY for testing
+
+stackBase      .FILL X4000      ; initial sttack pointer
+
+Main           LD R6,stackBase  ; initialize stack pointer
+               LD R0,option     ; select routine to test
+               BRZ testPrintNum ; option = 0 means test printNum
+
+               ADD R0,R0,#-1
+               BRZ testGetDigit ; option = 1 means test getDidit
+
+               ADD R0,R0,#-1
+               BRZ testDivRem   ; option = 2 means test divRem
+
+               HALT             ; invalid option if here
+
+testPrintNum                    ; call printNum(x, base)
+               LD R0,data2
+               PUSH R0          ; push base argument
+               LD R0,data1
+               PUSH R0          ; push value argument
+               JSR printNum
+               ADD R6,R6,#2     ; caller clean up 2 parameters
+               BR endTest
+
+testGetDigit                    ; call getChar(val)
+               LD R0,data1
+               PUSH R0          ; push argument (val to convert to ASCII)
+               JSR getDigit     ; 
+               POP R0           ; get the corresponding character
+               ADD R6,R6,#1     ; caller clean up 1 parameter
+               OUT              ; print the digit
+               NEWLN
+               BR endTest
+
+testDivRem                      ; call divRem(num, denom, *quotient, *remainder)
+               LEA R0,data2     ; get pointer to remainder (&data2)
+               PUSH R0
+               LEA R0,data1     ; get pointer to quotient (&data1)
+               PUSH R0
+               LD R0,data2
+               PUSH R0          ; push demoninator
+               LD R0,data1
+               PUSH R0          ; push numerator
+               JSR divRem       ; call routine
+               ADD R6,R6,#4     ; caller clean up 4 parameters
+
+endTest        LEA R0,endMsg
+               PUTS
+theEnd         HALT             ; look at data1/data2 for quotient/remainder
+
+                                ; useful constants
+endMsg         .STRINGz "\nTest complete!\n"
+
+negSign        .FILL    x2D     ; ASCII '-'
+digits         .STRINGZ "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ" ; up to base 36
+
+; end reserved section
+
+; ------------------------------------------------------------------------
+; Author: your name goes here
+; ------------------------------------------------------------------------
+;
+; C declaration: char getDigit (int val);
+
+getDigit                        ; callee setup
+
+                                ; code for getDigit
+
+                                ; callee cleanup
+               RET              ; return
+; ------------------------------------------------------------------------
+;
+; C declaration: void divRem(int num, int denom, int*quotient, int*remainder);
+
+divRem                          ; callee setup
+
+                                ; code for dicRem
+
+                                ; callee cleanup
+               RET              ; return
+; ------------------------------------------------------------------------
+;
+; C declaration: void printNum (int val, int base);
+
+printNum                        ; callee setup
+
+                                ; code for printNum
+
+                                ; callee cleanup
+               RET              ; return
+; ------------------------------------------------------------------------
+               .END
---
+++ P7/printnum.h
@@ -0,0 +1,70 @@
+#ifndef __PRINTNUM_H__
+#define __PRINTNUM_H__
+
+/*
+ * printnum.h - simple functions to print a number using recursion.
+ *
+ * "Copyright (c) 2013-15 by Your name goes here."
+ *
+ * Permission to use, copy, modify, and distribute this software and its
+ * documentation for any purpose, without fee, and without written
+ * agreement is hereby granted, provided that the above copyright notice
+ * and the following two paragraphs appear in all copies of this software.
+ *
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE TO ANY PARTY FOR DIRECT,
+ * INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
+ * OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF THE AUTHOR
+ * HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * THE AUTHOR SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE.  THE SOFTWARE PROVIDED HEREUNDER IS ON AN "AS IS"
+ * BASIS, AND THE AUTHOR NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT,
+ * UPDATES, ENHANCEMENTS, OR MODIFICATIONS."
+ */
+
+/** @file printnum.h
+ *  @brief Defines interface of printnum.c functions (do not modify)
+ *  @details This file defines the interface to a C file printnum.c that
+ *  you will complete. You will learn how to use the C language operators
+ *  for address-of (<b>&amp;</b>), and dereferencing pointers <b>*</b>). 
+ *  This will serve as a model when you implement the same functions in LC3
+ *  assembly language.
+ */
+
+/** A string of characters representing the digits to output for a particular
+ *  value. Defined in the file <code>testPrint.c</code>.
+ */
+extern char* digits;
+
+/** Return the character corresponding to the digit. 
+ *  Use <code>val</code> as an index into the string <code>digits</code>
+ *  @param val - a value in the range of <code>0 - (base-1)</code>
+ *  @return - character '0' .. '9' or 'A'..'Z'
+ */
+char getDigit (int val);
+
+/** Calculate both the quotient and remainder of a division and return the
+ *  values via pointers. You may use the C operators for division (<b>/</b>)
+ *  and modulus (<b>%</b>). However, you might want to write a loop to
+ *  using repeated subtraction do the calculations to help you understand how
+ *  you will implement this in LC3 assembly language (which has no operators
+ *  for division or modulus).
+ *  @param numerator - non-negative value for numerator
+ *  @param divisor - a positive value to divide by
+ *  @param quotient - a pointer to the location to store the result of division
+ *  @param remainder - a pointer to the location to store the remainder
+ */
+void divRem (int numerator, int divisor, int* quotient, int* remainder);
+
+/** Print a number in the specified base. Use the C call <code>putchar()</code>
+ *  to print a single character obtained using <code>getDigit()</code>.
+ *  This corresponds to the <code>OUT</code> instruction of the LC3 OS. You may
+ *  not use any other C I/O routine for printing. This function <b>MUST</b> be
+ *  implemented recursively.
+ *  @param x - the number to print
+ *  @param base - the base in which the number should be printed
+ */
+void printNum (int x, int base);
+
+#endif
